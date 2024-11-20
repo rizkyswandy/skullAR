f54c8b3a45513b7cec1cdffd5103b99c6834096b
Rizky Azmi Swandy <rizkyswandy@gmail.com>
2024-11-20 15:42:12 +0700

    Initial Commit

---
+++ SkullAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkullAR
+//
+//  Created by ILB on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkullAR/SkullARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkullARApp.swift
+//  SkullAR
+//
+//  Created by ILB on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SkullARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
